0978e47d4254a7e02a614bbdd817b6ecb547e528
Jonathan Rocha <jonx0037@gmail.com>
2024-12-08 09:57:09 -0600

    fix: update Twitter terms of service link in policy timeline component

--- src/components/policy-timeline.tsx
+++ src/components/policy-timeline.tsx
@@ -15,7 +15,7 @@ const PolicyTimeline: React.FC = () => {
       date: "August 19, 2021",
       title: "Twitter Terms of Service",
       description: "The terms governing the Twitter platform prior to X's acquisition",
-      impact: "Defined the rules and policies for Twitter users. See the full terms at <a href='https://help.x.com/en/rules-and-policies' target='_blank' rel='noopener noreferrer' className='text-indigo-600 underline'>help.x.com/en/rules-and-policies</a>"
+      impact: "Defined the rules and policies for Twitter users. See the full terms at <a href='help.twitter.com/en/rules-and-policies/twitter-terms-of-service' target='_blank' rel='noopener noreferrer' className='text-indigo-600 underline'>help.twitter.com/en/rules-and-policies/twitter-terms-of-service</a>"
     },
     {
       date: "January 2023",
